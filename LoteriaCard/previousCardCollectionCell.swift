f49f3c6a29f475c8a0d352f1933962940bb4da21
jdvilla94 <jdvilla43@gmail.com>
2023-10-31 08:37:14 -0500

    finsih updating

---
+++ LoteriaCard/previousCardCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  previousCardCollectionCell.swift
+//  LoteriaCard
+//
+//  Created by JD Villanueva on 10/25/23.
+//
+
+import UIKit
+
+class previousCardCollectionCell: UICollectionViewCell {
+
+
+    @IBOutlet var card: UIImageView!
+    
+}
